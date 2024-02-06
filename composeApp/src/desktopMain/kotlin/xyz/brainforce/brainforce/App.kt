db07a22b07248d3106caae900d6d41f1d8b8f465
Atri <atriusedm@gmail.com>
2024-02-05 20:15:31 -0500

    chore: Simplify project

--- composeApp/src/desktopMain/kotlin/xyz/brainforce/brainforce/App.kt
+++ composeApp/src/desktopMain/kotlin/xyz/brainforce/brainforce/App.kt
@@ -1,3 +1,5 @@
+package xyz.brainforce.brainforce
+
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
@@ -20,7 +22,6 @@ import org.jetbrains.compose.resources.painterResource
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
-        val greeting = remember { Greeting().greet() }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { showContent = !showContent }) {
                 Text("Click me!")
@@ -28,7 +29,7 @@ fun App() {
             AnimatedVisibility(showContent) {
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
                     Image(painterResource("compose-multiplatform.xml"), null)
-                    Text("Compose: $greeting")
+                    Text("Compose: Hello")
                 }
             }
         }
