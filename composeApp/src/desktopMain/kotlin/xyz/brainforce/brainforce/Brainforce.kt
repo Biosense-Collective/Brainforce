e4c85430738d5b4d5f1037235763baca11e3d53b
Atri <atriusedm@gmail.com>
2024-02-13 16:57:54 -0500

    chore: Add manager class for interfacing with the main config

--- composeApp/src/desktopMain/kotlin/xyz/brainforce/brainforce/Brainforce.kt
+++ composeApp/src/desktopMain/kotlin/xyz/brainforce/brainforce/Brainforce.kt
@@ -1,46 +1,39 @@
 package xyz.brainforce.brainforce
 
-import brainflow.BoardShim
 import brainflow.LogLevels
 import org.koin.core.annotation.Singleton
 import org.slf4j.LoggerFactory
 import xyz.brainforce.brainforce.api.data.file.BrainforceConfig
-import xyz.brainforce.brainforce.api.data.file.FileReference
+import xyz.brainforce.brainforce.api.manager.BrainforceConfigManager
 import xyz.brainforce.brainforce.api.service.ConnectionService
-import xyz.brainforce.brainforce.api.service.FileService
+import xyz.brainforce.brainforce.api.util.Logger
 import xyz.brainforce.brainforce.framework.Application
 import kotlin.time.Duration.Companion.seconds
 
 @Singleton
 class Brainforce(
-    private val fileService: FileService,
+    private val bfiConfigManager: BrainforceConfigManager,
     private val connectionService: ConnectionService,
 ) : Application {
 
     override fun initialize() {
         logger.info("Starting Brainforce...")
 
-        val bfiConfig = getBrainforceConfig()
-
+        val bfiConfig = bfiConfigManager
+            .getConfig()
+            .also(::enableConnectionService)
         if (bfiConfig.debug) {
-            BoardShim.set_log_level(LogLevels.LEVEL_DEBUG)
+            Logger.setLevel(LogLevels.LEVEL_DEBUG)
         }
 
-        enableConnectionService(bfiConfig)
         logger.info("Brainforce started!")
         Thread.currentThread().join()
     }
-
-    private fun getBrainforceConfig(): BrainforceConfig = fileService
-        .loadFile(FileReference.MainConfig, BrainforceConfig.serializer(), defaultConfig)
 
     private fun enableConnectionService(config: BrainforceConfig) = connectionService
         .enable(config.asBoardShim(), config.timeout.seconds)
 
     companion object {
-
-        private val defaultConfig: BrainforceConfig =
-            BrainforceConfig()
 
         private val logger = LoggerFactory
             .getLogger(Brainforce::class.java)
--- composeApp/src/desktopMain/kotlin/xyz/brainforce/brainforce/api/util/Logger.kt
+++ composeApp/src/desktopMain/kotlin/xyz/brainforce/brainforce/api/util/Logger.kt
@@ -5,9 +5,15 @@ import brainflow.LogLevels
 
 object Logger {
 
+    fun debug(message: String) = BoardShim.log_message(LogLevels.LEVEL_DEBUG, message)
+
     fun info(message: String) = BoardShim.log_message(LogLevels.LEVEL_INFO, message)
 
     fun warn(message: String) = BoardShim.log_message(LogLevels.LEVEL_WARN, message)
 
-    fun debug(message: String) = BoardShim.log_message(LogLevels.LEVEL_DEBUG, message)
+    fun error(message: String) = BoardShim.log_message(LogLevels.LEVEL_ERROR, message)
+
+    fun fatal(message: String) = BoardShim.log_message(LogLevels.LEVEL_CRITICAL, message)
+
+    fun setLevel(level: LogLevels) = BoardShim.set_log_level(level)
 }
