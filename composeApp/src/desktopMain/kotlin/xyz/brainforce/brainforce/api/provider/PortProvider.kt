9c450ea0eb689f675b4d69dc8ed0f4aa3f0a6565
Atri <atriusedm@gmail.com>
2024-02-14 01:00:13 -0500

    fix: Exception due to generating too many outgoing OSC ports via provider

--- composeApp/src/desktopMain/kotlin/xyz/brainforce/brainforce/api/provider/PortProvider.kt
+++ composeApp/src/desktopMain/kotlin/xyz/brainforce/brainforce/api/provider/PortProvider.kt
@@ -3,16 +3,26 @@ package xyz.brainforce.brainforce.api.provider
 import com.illposed.osc.transport.OSCPortOut
 import org.koin.core.annotation.Singleton
 import xyz.brainforce.brainforce.api.manager.BrainforceConfigManager
+import xyz.brainforce.brainforce.api.util.Logger
 import java.net.InetAddress
+import java.util.Objects
 
 @Singleton
 class PortProvider(
     private val brainforceConfig: BrainforceConfigManager,
 ) {
+    private val oscOutCache = mutableMapOf<Int, OSCPortOut>()
 
     fun provide(): OSCPortOut {
         val config = brainforceConfig.getConfig()
-        val address = InetAddress.getByName(config.ipAddress)
-        return OSCPortOut(address, config.oscPort)
+        val port = config.oscPort
+        val ip = config.ipAddress
+        val hash = Objects.hash(port, ip)
+
+        return oscOutCache.computeIfAbsent(hash) {
+            Logger.info("Generating fresh OSC transport point at '$ip' port $port")
+            val address = InetAddress.getByName(ip)
+            OSCPortOut(address, port)
+        }
     }
 }
